ece915eda60bda841aa77fd3b55e94d2353e0797
Ronald Espinoza <ronaldplazae95@gmail.com>
2026-07-13 17:51:59 -0400

    Fix: Use circuitbreaker.New() instead of NewBreaker()

--- api/internal/senders/breakers.go
+++ api/internal/senders/breakers.go
@@ -1,10 +1,14 @@
 package senders
 
-import "harmony-api/internal/circuitbreaker"
+import (
+	"time"
+
+	"harmony-api/internal/circuitbreaker"
+)
 
 var (
-	whatsappBreaker   = circuitbreaker.NewBreaker(5, 60)
-	messengerBreaker  = circuitbreaker.NewBreaker(5, 60)
-	instagramBreaker  = circuitbreaker.NewBreaker(5, 60)
-	telegramBreaker   = circuitbreaker.NewBreaker(5, 60)
+	whatsappBreaker   = circuitbreaker.New(5, 60*time.Second)
+	messengerBreaker  = circuitbreaker.New(5, 60*time.Second)
+	instagramBreaker  = circuitbreaker.New(5, 60*time.Second)
+	telegramBreaker   = circuitbreaker.New(5, 60*time.Second)
 )
